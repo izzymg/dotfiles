ef05bbbd5cb37ae3038954bd7b969384795176ad
izzymg <izzymhg@gmail.com>
2024-04-14 13:49:10 +1200

    remove wallpaper code

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -119,21 +119,6 @@ local tasklist_buttons = gears.table.join(
 		awful.menu.client_list({ theme = { width = 250 } })
 	end)
 )
-
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 -- Update task list widgets 
 function task_update(w, buttons, label, data, objects) 
 	common.list_update(w, buttons, label, data, objects)
@@ -145,9 +130,6 @@ end
 
 
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    set_wallpaper(s)
-
     -- Each screen has its own tag table.
     awful.tag({ "1", "2"}, s, awful.layout.layouts[1])
 
